0740dd84368b9507223ed385c43d7736ad9f6e05
fisayo-dev <olufisayobadina@gmail.com>
2025-01-14 21:14:13 +0100

    create route structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "@/components/ui/button";
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import {
+  Signup,
+  Login,
+  PageNotFound,
+  VoltPage
+} from "./pages";
+import AppOutlet from "./outlets/AppOutlet";
+import PrivateRoutes from "./utils/PrivateRoutes";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a className="text-1xl" href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <Button>Hi</Button>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="" element={<PrivateRoutes />}>
+          <Route path="/whiteboard/board" element={<Board />} />
+          <Route path="" element={<AppOutlet />}>
+            <Route path="/volt" element={<VoltPage />} />
+          </Route>
+        </Route>
+      </>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
