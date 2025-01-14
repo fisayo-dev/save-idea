2f97be97e500660ac770e593bd5520e5152ddf2a
fisayo-dev <olufisayobadina@gmail.com>
2025-01-14 22:33:08 +0100

    create, implement form side component

--- client/src/components/Auth/FormSide.jsx
+++ client/src/components/Auth/FormSide.jsx
@@ -1,7 +1,17 @@
-const FormSide = () => {
+import landing_image from "../../assets/vectors/landing_blub.png";
+const FormSide = ({ children }) => {
   return (
-    <div>FormSide</div>
-  )
-}
+    <div className="grid grid-cols-2 overflow-hidden">
+      <div className="bg-yellow-100 h-[100vh]">
+        <div className="grid place-items-center h-full">
+          <img src={landing_image} className="h-[500px] w-[500px]" alt="" />
+        </div>
+      </div>
+      <div className="h-[100vh]">
+        <div className="grid place-items-center h-full">{children}</div>
+      </div>
+    </div>
+  );
+};
 
-export default FormSide
+export default FormSide;
--- client/src/pages/public/Login.jsx
+++ client/src/pages/public/Login.jsx
@@ -1,7 +1,12 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { FormSide } from "../../components";
 
-export default Login
+const Login = () => {
+  return <FormSide>
+    <div className="grid gap-10">
+      <h2>How are you?</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia aliquam nulla ut!</p>
+    </div>
+  </FormSide>;
+};
+
+export default Login;
