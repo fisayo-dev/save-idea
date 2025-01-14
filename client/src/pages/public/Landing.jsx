2b10d76e3451134c3d16266b14e5236c57be33f4
fisayo-dev <olufisayobadina@gmail.com>
2025-01-14 22:13:45 +0100

    add icons to btns

--- client/src/pages/public/Landing.jsx
+++ client/src/pages/public/Landing.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import { PiIcon, SearchIcon } from "lucide-react";
-import landing_blub from '../../assets/vectors/landing_blub.png'
+import { Rocket, SearchIcon } from "lucide-react";
+import landing_blub from "../../assets/vectors/landing_blub.png";
 import { useEffect } from "react";
-import { Header } from '../../components'
+import { Header } from "../../components";
 
 const Landing = () => {
   const fetchData = async () => {
@@ -26,19 +26,23 @@ const Landing = () => {
           <div className="grid md:flex md:my-0 my-10 gap-5 md:justify-between items-center">
             <div className="grid gap-4 md:text-left md:place-items-start place-items-center text-center">
               <h2 className="lg:text-5xl text-4xl capitalize font-bold">
-              Unleash Your Creativity, Empower Your Ideas 
+                Unleash Your Creativity, Empower Your Ideas
               </h2>
               <p className="text-[0.82rem] sm:text-[1rem]">
-              Capture, organize, and act on your inspirations seamlessly—because your ideas deserve more than just a fleeting moment.
+                Capture, organize, and act on your inspirations
+                seamlessly—because your ideas deserve more than just a fleeting
+                moment.
               </p>
               <div className="flex my-3 gap-2 place-items-center items-center justify-center ">
                 <Link to="/signup">
                   <button className="shadow-md text-[0.8rem] md:text-[1rem] px-4 py-3 flex items-center gap-2 border border-gray-500 rounded-full bg-yellow hover-dark-bg-yellow">
+                    <Rocket />
                     <p>Get Started</p>
                   </button>
                 </Link>
                 <Link to="/signup">
                   <button className="shadow-md text-[0.8rem] md:text-[1rem] px-4 py-3 flex gap-2 items-center rounded-full border border-gray-500 hover-dark-bg-yellow ">
+                    <SearchIcon />
                     <p>Explore Features</p>
                   </button>
                 </Link>
