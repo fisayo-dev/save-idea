e5d4e1a9ddef8d64d167adf44b33e8a87ffa5a77
fisayo-dev <olufisayobadina@gmail.com>
2025-01-14 21:35:56 +0100

    create, route public page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,11 @@
 // Public Routes
-
 import Landing from "./public/Landing";
 import Login from "./public/Login";
 import Signup from "./public/Signup";
+import PageNotFound from "./public/PageNotFound";
 
 // Private Routes
 import Volt from "./private/Volt";
 
-export { Landing, Login, Signup, Volt }
+export { Landing, Login, Signup, Volt, PageNotFound }
 
