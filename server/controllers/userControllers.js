a903f0e27e7b19ac2238391cd0fef273ae59609f
fisayo-dev <olufisayobadina@gmail.com>
2025-01-18 01:39:09 +0100

    create user controllers

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,17 @@
+const signupUser = async((req, res) => {
+    
+})
+const loginUser = async((req, res) => {
+    
+})
+const getUserDetails = async((req, res) => {
+    
+})
+const deleteUser = async((req, res) => {
+    
+})
+const editUser = async((req, res) => {
+    
+})
+
+export { loginUser, getUserDetails, deleteUser, editUser, signupUser }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { editUser,signupUser,loginUser,deleteUser,getUserDetails } from "../controllers/userControllers";
 
 const router = express.Router()
 
@@ -7,5 +8,6 @@ router.post('/signup', signupUser)
 router.post('/login', loginUser)
 router.delete('/delete', deleteUser)
 router.delete('/:id', getUserDetails)
+router.put('/:id', editUser)
 
 export default router
