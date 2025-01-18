77369b2d34df44f0bb919ed2b742cfc0ebb5669c
fisayo-dev <olufisayobadina@gmail.com>
2025-01-18 01:15:52 +0100

    setting up project

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.PORT;
 
-const app = express()
+const app = express();
+
+
+app.use(cors())
+app.route('')
 
 app.listen(PORT, () => {
     console.log('Server successfully started')
