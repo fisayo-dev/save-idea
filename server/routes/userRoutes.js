e167b495b440f99c1f946ae76a0531392893d68d
fisayo-dev <olufisayobadina@gmail.com>
2025-01-18 01:40:14 +0100

    update delete uer route to include only id slug

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 // Use routes
 router.post('/signup', signupUser)
 router.post('/login', loginUser)
-router.delete('/delete', deleteUser)
+router.delete('/:id', deleteUser)
 router.delete('/:id', getUserDetails)
 router.put('/:id', editUser)
 
