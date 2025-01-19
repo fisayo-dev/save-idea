8e6636b1d77a23f40242e622878bc32732575ec4
fisayo-dev <olufisayobadina@gmail.com>
2025-01-19 12:32:06 +0100

    add form card resposniveness

--- client/src/components/Auth/FormSide.jsx
+++ client/src/components/Auth/FormSide.jsx
@@ -1,21 +1,21 @@
 import landing_image from "../../assets/vectors/landing_blub.png";
 const FormSide = ({ children, formImagePosition }) => {
   return (
-    <div className="grid grid-cols-2 overflow-hidden">
+    <div className="grid xl:grid-cols-2 overflow-hidden">
       {formImagePosition == "left" && (
-        <div className="bg-yellow-100 h-[100vh]">
+        <div className="hidden xl:grid bg-yellow-100 h-[100vh]">
           <div className="grid place-items-center h-full">
             <img src={landing_image} className="h-[500px] w-[500px]" alt="" />
           </div>
         </div>
       )}
-      <div className="h-[100vh]">
+      <div className="h-[100vh] max-w-50 mx-auto px-10">
         <div className="flex flex-col justify-center h-full p-2  gap-8">
           {children}
         </div>
       </div>
       {formImagePosition == "right" && (
-        <div className="bg-yellow-100 h-[100vh]">
+        <div className="hidden xl:grid bg-yellow-100 h-[100vh]">
           <div className="grid place-items-center h-full">
             <img src={landing_image} className="h-[500px] w-[500px]" alt="" />
           </div>
