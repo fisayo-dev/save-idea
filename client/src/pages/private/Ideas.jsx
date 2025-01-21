80c7675bfedfb10895b5754b3619f334e79e6129
fisayo-dev <olufisayobadina@gmail.com>
2025-01-21 23:28:36 +0100

    create idea card layout

--- client/src/pages/private/Ideas.jsx
+++ client/src/pages/private/Ideas.jsx
@@ -2,6 +2,40 @@ import { SearchNormal } from "iconsax-react";
 import { LayoutGrid, LayoutList } from "lucide-react";
 
 const Ideas = () => {
+  const ideas = [
+    {
+      title: "Idea 1",
+      description:
+        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
+      date_created: "12/04/2024",
+      img: "12",
+      category: "science",
+    },
+    {
+      title: "Idea 2",
+      description:
+        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket.Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. ",
+      date_created: "12/04/2024",
+      img: "12",
+      category: "science",
+    },
+    {
+      title: "Idea 3",
+      description:
+        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
+      date_created: "12/04/2024",
+      img: "12",
+      category: "science",
+    },
+    {
+      title: "Idea 4",
+      description:
+        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
+      date_created: "12/04/2024",
+      img: "12",
+      category: "science",
+    },
+  ];
   return (
     <div className="app-container">
       <div className="py-6">
@@ -23,6 +57,23 @@ const Ideas = () => {
               <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
                 <LayoutList className="h-6 w-6" />
               </div>
+            </div>
+          </div>
+          <div className="my-2">
+            <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4 items-start">
+              {ideas.map((idea, index) => (
+                <div key={index} className="grid  gap-2">
+                  <div className="h-[150px] w-full  rounded-xl bg-gray-200"></div>
+                  <div>
+                    <h2 className="font-bold">{idea.title}</h2>
+                    <p className="text-sm">
+                      {idea.description.length > 50
+                        ? `${idea.description.substring(0, 50)}...`
+                        : idea.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
