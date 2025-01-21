7cfc40f8e579ed9a1ba56302a9133f4554eeefaa
fisayo-dev <olufisayobadina@gmail.com>
2025-01-21 21:51:47 +0100

    creating card layout

--- client/src/pages/private/Profile.jsx
+++ client/src/pages/private/Profile.jsx
@@ -1,7 +1,22 @@
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="app-container">
+      <div className="my-10">
+        <div className="grid gap-4">
+          <div className="rounded-xl p-4 border-2 border-gray-400 bg-gray-100">
+            <div className="grid md:flex items-center gap-6">
+              <div className="md:mx-0 mx-auto h-28 w-28 rounded-full bg-gray-300" />
+              <div className="flex flex-col gap-1 md:items-start items-center ">
+                <h2 className="font-bold text-2xl">Fisayo Obadina</h2>
+                <p className="">olufisayobadina@gmail.com</p>
+                <p className="text-sm">Joined Yesterday</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
