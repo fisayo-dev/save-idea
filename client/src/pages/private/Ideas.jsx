0ae25f46e7e0d88a0c7db970b34831c8fb71a1c6
fisayo-dev <olufisayobadina@gmail.com>
2025-01-21 23:11:43 +0100

    create ideas page layout

--- client/src/pages/private/Ideas.jsx
+++ client/src/pages/private/Ideas.jsx
@@ -1,6 +1,32 @@
+import { SearchNormal } from "iconsax-react";
+import { LayoutGrid, LayoutList } from "lucide-react";
+
 const Ideas = () => {
   return (
-    <div>
+    <div className="app-container">
+      <div className="py-6">
+        <div className="grid gap-4">
+          <h2 className="text-4xl font-bold text-center">Your Ideas</h2>
+          <div className="flex items-center gap-4">
+            <div className="flex bg-gray-100 py-4 px-6 rounded-full items-center gap-2 w-full ">
+              <input
+                type="text"
+                className="w-full"
+                placeholder="Search for all your ideas"
+              />
+              <SearchNormal className="h-6 w-6" />
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
+                <LayoutGrid className="h-6 w-6" />
+              </div>
+              <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
+                <LayoutList className="h-6 w-6" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
