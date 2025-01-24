48c3c8dfdb5c228f4b261c431fb64d44a434abc4
fisayo-dev <olufisayobadina@gmail.com>
2025-01-24 23:19:13 +0100

    change ui for individual idea page

--- client/src/pages/private/SingleIdea.jsx
+++ client/src/pages/private/SingleIdea.jsx
@@ -32,24 +32,24 @@ const SingleIdea = () => {
         {!error && !idea && <div className="text-blue-500">Loading...</div>}
         {!error && idea && (
           <div className="grid gap-6 p-2">
-            <div className="grid gap-3">
-              <label className="border-b text-sm ">Title</label>
+            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+              <label className="text-sm ">Title</label>
               <h2 className="text-3xl font-bold">{idea.title}</h2>
             </div>
-            <div className="grid gap-3">
-              <label className="border-b text-sm ">Description</label>
+            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+              <label className="text-sm ">Description</label>
               <h2 className="text-3xl">{idea.description}</h2>
             </div>
-            <div className="grid gap-3">
-              <label className="border-b text-sm ">Category</label>
+            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+              <label className="text-sm ">Category</label>
               <h2 className="text-3xl">{idea.category}</h2>
             </div>
-            <div className="grid gap-3">
-              <label className="border-b text-sm ">Problem to solve</label>
+            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+              <label className="text-sm ">Problem to solve</label>
               <h2 className="text-3xl">{idea.problem_to_solve}</h2>
             </div>
-            <div className="grid gap-3">
-              <label className="border-b text-sm ">Inspirational source</label>
+            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+              <label className="text-sm ">Inspirational source</label>
               <h2 className="text-3xl">{idea.inspiration_source}</h2>
             </div>
           </div>
