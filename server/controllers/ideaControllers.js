d6db7916ed775da0b2aefde7172ca6f1498df5af
fisayo-dev <olufisayobadina@gmail.com>
2025-01-24 14:53:58 +0100

    created controllers for ideas

---
+++ server/controllers/ideaControllers.js
@@ -0,0 +1,15 @@
+const createIdea = async (req, res) => { 
+    
+}
+const deleteIdea = async (req, res) => { 
+
+}
+const getIdeas = async (req, res) => { 
+
+}
+const getSingleIdea = async (req, res) => { 
+
+}
+const updateIdea = async (req, res) => { 
+
+}
