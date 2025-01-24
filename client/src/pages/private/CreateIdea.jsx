b92664cd2aa5c9bcd29e5bdd716d555d955401c1
fisayo-dev <olufisayobadina@gmail.com>
2025-01-24 21:17:45 +0100

    create form input for create idea

--- client/src/pages/private/CreateIdea.jsx
+++ client/src/pages/private/CreateIdea.jsx
@@ -1,7 +1,28 @@
+import { Input } from "../../components/ui/input";
+
+
 const CreateIdea = () => {
   return (
-    <div>CreateIdea</div>
-  )
-}
+    <div className="2xl:container mx-auto">
+      <div className="py-6">
+        <div className="grid gap-4">
+          <h2 className="text-4xl font-bold text-center">
+            What's on your mind today 🤔
+          </h2>
+          <form className="grid gap-3">
+            <div className="grid gap-3">
+              <label>Title</label>
+              <Input type="text" placeholder="Give your idea a name" />
+            </div>
+            <div className="grid gap-3">
+              <label>Description</label>
+              <Input type="text" placeholder="Give your idea a name" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreateIdea
+export default CreateIdea;
