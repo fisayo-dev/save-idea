cf260b57d5c5876ecc0438f83c3f54325d4e1624
fisayo-dev <olufisayobadina@gmail.com>
2025-01-24 23:24:48 +0100

    create layout for action btns on idea

--- client/src/pages/private/SingleIdea.jsx
+++ client/src/pages/private/SingleIdea.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../../axiosConfig";
 import { useAuth } from "../../contexts/AuthContext";
+import { Button } from "../../components/ui/button";
+import { Back, Trash } from "iconsax-react";
+import { Pencil } from "lucide-react";
 
 const SingleIdea = () => {
   const { id } = useParams();
@@ -31,26 +34,42 @@ const SingleIdea = () => {
         {error && <div className="text-red-500">{error}</div>}
         {!error && !idea && <div className="text-blue-500">Loading...</div>}
         {!error && idea && (
-          <div className="grid gap-6 p-2">
-            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
-              <label className="text-sm ">Title</label>
-              <h2 className="text-3xl font-bold">{idea.title}</h2>
+          <div className="grid gap-4">
+            <div className="flex items-center justify-between">
+              <Button className="flex items-center gap-2">
+                <Back className="h-8 w-8"/>
+                <p>Back</p>
+              </Button>
+              <div className="flex items-center justify-between gap-2">
+                <div className="bg-gray-200 cursor-pointer hover:bg-gray-300 rounded-full p-3">
+                  <Pencil />
+                </div>
+                <div className="bg-gray-200 cursor-pointer hover:bg-gray-300 rounded-full p-3">
+                  <Trash />
+                </div>
+              </div>
             </div>
-            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
-              <label className="text-sm ">Description</label>
-              <h2 className="text-3xl">{idea.description}</h2>
-            </div>
-            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
-              <label className="text-sm ">Category</label>
-              <h2 className="text-3xl">{idea.category}</h2>
-            </div>
-            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
-              <label className="text-sm ">Problem to solve</label>
-              <h2 className="text-3xl">{idea.problem_to_solve}</h2>
-            </div>
-            <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
-              <label className="text-sm ">Inspirational source</label>
-              <h2 className="text-3xl">{idea.inspiration_source}</h2>
+            <div className="grid gap-6 p-2">
+              <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+                <label className="text-sm ">Title</label>
+                <h2 className="text-3xl font-bold">{idea.title}</h2>
+              </div>
+              <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+                <label className="text-sm ">Description</label>
+                <h2 className="text-3xl">{idea.description}</h2>
+              </div>
+              <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+                <label className="text-sm ">Category</label>
+                <h2 className="text-3xl">{idea.category}</h2>
+              </div>
+              <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+                <label className="text-sm ">Problem to solve</label>
+                <h2 className="text-3xl">{idea.problem_to_solve}</h2>
+              </div>
+              <div className="grid gap-3 p-4 rounded-xl bg-gray-200">
+                <label className="text-sm ">Inspirational source</label>
+                <h2 className="text-3xl">{idea.inspiration_source}</h2>
+              </div>
             </div>
           </div>
         )}
