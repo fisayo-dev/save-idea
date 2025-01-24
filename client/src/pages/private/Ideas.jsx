b345f0705231ab1c379d72e606a494bf897b0bc1
fisayo-dev <olufisayobadina@gmail.com>
2025-01-24 20:54:37 +0100

    implementign read operation for ideas

--- client/src/pages/private/Ideas.jsx
+++ client/src/pages/private/Ideas.jsx
@@ -6,107 +6,90 @@ import { useAuth } from "../../contexts/AuthContext";
 
 const Ideas = () => {
   const { user } = useAuth();
-  console.log(user);
-  // const [ideasLoading, setIdeasLoading] = useState([]);
+  const [ideasList, setIdeasList] = useState([]);
+  const [error, setError] = useState(null); 
 
   const fetchIdeas = async () => {
     try {
       const response = await axiosInstance.get(`/ideas/${user}`);
-      const data = response.data; // No need for `await` here
-      console.log(data);
+      const data = response.data;
+      setIdeasList(data.ideas);
+      setError(null); 
     } catch (err) {
-      console.log("Error:", err.message);
+      setIdeasList([]);
+      setError(err.message || "An unknown error occurred");
     }
   };
+
   useEffect(() => {
     fetchIdeas();
   }, []);
 
-  const ideas = [
-    {
-      title: "Idea 1",
-      description:
-        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
-      date_created: "12/04/2024",
-      img: "12",
-      category: "science",
-      status: "Done",
-    },
-    {
-      title: "Idea 2",
-      description:
-        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket.Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. ",
-      date_created: "12/04/2024",
-      img: "12",
-      category: "science",
-      status: "Still in progress",
-    },
-    {
-      title: "Idea 3",
-      description:
-        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
-      date_created: "12/04/2024",
-      img: "12",
-      category: "science",
-      status: "Almost done",
-    },
-    {
-      title: "Idea 4",
-      description:
-        "Lorem ispum dor los sit aket. Lorem ispum dor los sit aket. Lorem ispum dor los sit aket ",
-      date_created: "12/04/2024",
-      img: "12",
-      category: "science",
-      status: "unfinished",
-    },
-  ];
   return (
     <div className="2xl:container mx-auto">
       <div className="py-6">
         <div className="grid gap-4">
           <h2 className="text-4xl font-bold text-center">Your Ideas</h2>
-          <div className="flex items-center gap-4">
-            <div className="flex bg-gray-100 py-4 px-6 rounded-full items-center gap-2 w-full ">
-              <input
-                type="text"
-                className="w-full"
-                placeholder="Search for all your ideas"
-              />
-              <SearchNormal className="h-6 w-6" />
+
+          {/* Display error if any */}
+          {error && (
+            <div className="text-center text-red-600">
+              <p>{error}</p>
             </div>
-            <div className="flex items-center gap-2">
-              <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
-                <LayoutGrid className="h-6 w-6" />
-              </div>
-              <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
-                <LayoutList className="h-6 w-6" />
-              </div>
-            </div>
-          </div>
-          <div className="my-2">
-            <div className="grid  2xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 md:gap-6 gap-8 items-start">
-              {ideas.map((idea, index) => (
-                <div key={index} className="grid gap-2">
-                  <div className="h-[150px] w-full cursor-pointer rounded-xl hover:bg-gray-300 bg-gray-200"></div>
-                  <div>
-                    <h2 className="font-bold text-xl">{idea.title}</h2>
-                    <p className="">
-                      {idea.description.length > 50
-                        ? `${idea.description.substring(0, 50)}...`
-                        : idea.description}
-                    </p>
+          )}
+
+          {/* Display message when no ideas are available */}
+          {!error && ideasList.length === 0 && (
+            <p className="text-center">You don't have any ideas yet</p>
+          )}
+
+          {/* Display ideas when available */}
+          {ideasList.length > 0 && (
+            <>
+              <div className="flex items-center gap-4">
+                <div className="flex bg-gray-100 py-4 px-6 rounded-full items-center gap-2 w-full ">
+                  <input
+                    type="text"
+                    className="w-full"
+                    placeholder="Search for all your ideas"
+                  />
+                  <SearchNormal className="h-6 w-6" />
+                </div>
+                <div className="flex items-center gap-2">
+                  <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
+                    <LayoutGrid className="h-6 w-6" />
                   </div>
-                  <div className="flex text-sm text-gray-700 items-center justify-between">
-                    <div className="flex items-center gap-1">
-                      <LucideTarget className="h-5 w-5 text-black" />
-                      <p className="capitalize">{idea.status}</p>
-                    </div>
-                    <p>{idea.date_created}</p>
+                  <div className="p-3 rounded-full cursor-pointer hover:bg-gray-200">
+                    <LayoutList className="h-6 w-6" />
                   </div>
                 </div>
-              ))}
-            </div>
-          </div>
+              </div>
+              <div className="my-2">
+                <div className="grid  2xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 md:gap-6 gap-8 items-start">
+                  {ideasList.map((idea, index) => (
+                    <div key={index} className="grid gap-2">
+                      <div className="h-[150px] w-full cursor-pointer rounded-xl hover:bg-gray-300 bg-gray-200"></div>
+                      <div>
+                        <h2 className="font-bold text-xl">{idea.title}</h2>
+                        <p className="">
+                          {idea.description.length > 50
+                            ? `${idea.description.substring(0, 50)}...`
+                            : idea.description}
+                        </p>
+                      </div>
+                      <div className="flex text-sm text-gray-700 items-center justify-between">
+                        <div className="flex items-center gap-1">
+                          <LucideTarget className="h-5 w-5 text-black" />
+                          <p className="capitalize">{idea.status}</p>
+                        </div>
+                        <p>{idea.date_created}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
