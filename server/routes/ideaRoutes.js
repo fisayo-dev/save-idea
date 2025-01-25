c56779010aa7b3e711fcd1d79b96f0a75c9d7847
fisayo-dev <olufisayobadina@gmail.com>
2025-01-25 22:28:57 +0100

    added auth middleware for security

--- server/routes/ideaRoutes.js
+++ server/routes/ideaRoutes.js
@@ -5,11 +5,11 @@ const router = express.Router();
 import { getIdeas, getSingleIdea, createIdea, updateIdea, deleteIdea } from '../controllers/ideaControllers.js'   // Import the functions from the ideasController.js file
 import authMiddleware from '../middlewares/authMiddleware.js';
 
-router.get('/:creator_id', getIdeas);  // GET request to fetch all ideas
-router.get('/:id/creator/:creator_id', getSingleIdea);  // GET request to fetch a single idea
-router.post('/', createIdea);  // POST request to create a new idea
-router.put('/:id', updateIdea);  // PUT request to update an idea
-router.delete('/:id', deleteIdea);  // DELETE request to delete an idea
+router.get('/:creator_id', authMiddleware, getIdeas);  // GET request to fetch all ideas
+router.get('/:id/creator/:creator_id', authMiddleware, getSingleIdea);  // GET request to fetch a single idea
+router.post('/', authMiddleware, createIdea);  // POST request to create a new idea
+router.put('/:id', authMiddleware, updateIdea);  // PUT request to update an idea
+router.delete('/:id', authMiddleware, deleteIdea);  // DELETE request to delete an idea
 
 
 export default router;
