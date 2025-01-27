325a1dd02ddef4ea67caa318e510aff031bbef61
fisayo-dev <olufisayobadina@gmail.com>
2025-01-27 23:00:49 +0100

    removed code block

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ const corsOptions = {
       ? 'https://saveideaapp.vercel.app/' 
       : 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, 
 };
   
